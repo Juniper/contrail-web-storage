1e858980ebe129e146f3f397ee3af2ec8f1d1cb1
Sarin Kizhakkepurayil <sarink@juniper.net>
2014-06-06 19:39:42 -0700

    loadLogs looks for infraMonitorUrl QUERY object. adding this with the default api end point. this needs to be changed to point to storage log api endpoint once done.

--- webroot/monitor/infra/common/ui/js/monitor_infra_storage_constants.js
+++ webroot/monitor/infra/common/ui/js/monitor_infra_storage_constants.js
@@ -12,9 +12,14 @@ globalObj['dataSources']['storageNodeDS'] = {
 
 var storageNodeTabs = ['details', 'disks', 'monitor'];
 var storageNodesDashboardChartInitialized = false;
-summaryChartsInitializationStatus['storageNode'] =false;
+var storageSummaryChartsInitializationStatus = {storageNode:false};
+
 
 var monitorInfraStorageUrls = {
     STORAGENODES_SUMMARY    : '/api/admin/monitor/infrastructure/storagenodes/summary',
     STORAGENODE_DETAILS     : '/api/admin/monitor/infrastructure/storagenodes/details?hostname={0}'
+}
+
+var monitorInfraUrls = {
+    QUERY                       : '/api/admin/reports/query'
 }
