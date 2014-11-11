094c18dede1b5113de5fd8d6e182b0f2605865a3
Suresh Akula <surakula@juniper.net>
2014-11-10 17:49:10 -0800

    Update storage.global.js
    Added summary job constants

--- webroot/common/config/storage.global.js
+++ webroot/common/config/storage.global.js
@@ -4,8 +4,15 @@
 
 var global = {};
 
+global.STR_JOB_TYPE_CACHE = 'cache';
+
 /* Storage Cluster golbal variables */
 global.STR_STORAGE_TYPE_CLUSTER = 'getStorageClusterStatus';
 
-module.exports = global;
+global.STR_GET_STORAGE_SUMMARY = 'getStorageSummary';
 
+global.STORAGE_SUMM_JOB_REFRESH_TIME = 5 * 60 * 1000;
+
+global.STORAGE_STATUS_JOB_REFRESH_TIME = 5 * 60 * 1000;
+
+module.exports = global;
