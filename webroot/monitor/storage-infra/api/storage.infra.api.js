1f2e24d39c7234d805cac28c7d043aca2f18d9a2
surakula <surakula@juniper.net>
2014-12-05 18:47:21 -0800

    Closes-Bug: #1399014 Need simple Ceph pool info for OSDs in UI Fix: adding a new property to each and every OSD’s list i.e. ”ceph_crush_name”: “hdd” or “ssd” to summary default tree and added only to osd’s belong to ssd-root or hdd-root, (for default-root osds which are not part of ssd/hdd root there will be no property). mapping for ceph crush name —> Pool Names hdd = volumes_hdd, volumes, images ssd = volumes_ssd default= volumes, images Test: checked with Summary tree with this new property for all OSD's
    Change-Id: I7114890d296f3efe01eae99540a9f3579fe8787d

--- webroot/monitor/storage-infra/api/storage.infra.api.js
+++ webroot/monitor/storage-infra/api/storage.infra.api.js
@@ -3,8 +3,10 @@
  */
 var storageApi= require('../../../common/api/storage.api.constants');
 var storageGlobal = require('../../../common/config/storage.global');
-var cacheApi = require(process.mainModule.exports["corePath"] + '/src/serverroot/web/core/cache.api'),
-    commonUtils = require(process.mainModule.exports["corePath"]  + '/src/serverroot/utils/common.utils'),
+var cacheApi = require(process.mainModule.exports["corePath"] +
+    '/src/serverroot/web/core/cache.api'),
+    commonUtils = require(process.mainModule.exports["corePath"]  +
+                    '/src/serverroot/utils/common.utils'),
     storageRest= require('../../../common/api/storage.rest.api'),
     global = require(process.mainModule.exports["corePath"] + '/src/serverroot/common/global'),
     config = require(process.mainModule.exports["corePath"] + '/config/config.global.js'),
@@ -78,13 +80,15 @@ function parseStorageTopologyTree(osdJSON, callback){
     var osdDump= osdJSON[2];
     var status= osdJSON[3];
 
-    var rootMap = jsonPath(osdTree, "$..nodes[?(@.name=='default')]");
+    //var rootMap = jsonPath(osdTree, "$..nodes[?(@.name=='default')]");
+    var rootMap = jsonPath(osdTree, "$..nodes[?(@.type=='root')]");
     var hostMap = jsonPath(osdTree, "$..nodes[?(@.type=='host')]");
     var tOSDs = jsonPath(osdTree, "$..nodes[?(@.type=='osd')]");
     var osds = jsonPath(osdDump, "$..osds");
     var monsJSON = jsonPath(monsApi.consolidateMonitors(status), "$..monitors")[0];
-    var mons_total = jsonPath(monsApi.consolidateMonitors(status), "$..mons_total")[0];
-    var mons_active = jsonPath(monsApi.consolidateMonitors(status), "$..mons_active")[0];
+    var mons_total = jsonPath(status, "$..monmap.mons.length")[0];
+    var mons_active = jsonPath(status, "$..health.health_services..mons.length")[0];
+
     if (osds != undefined && osds.length > 0) {
         var osdName='undefined';
         for(i=0; i < tOSDs.length;i++){
@@ -99,19 +103,54 @@ function parseStorageTopologyTree(osdJSON, callback){
             hostMap = parseMonitorWithHost(monsJSON, hostMap);
             osdApi.getAvgBWHostToOSD(osds,hostMap, function(osds){
                 hostMap = osdApi.parseHostFromOSD(hostMap, osds, version, true);
-                osdList.topology = parseRootFromHost(rootMap, hostMap);
+                //osdList.topology = parseRootFromHost(rootMap, hostMap);
+                var treeList ={};
+                treeList= parseRootFromHost(rootMap, hostMap);
+                var defaultList = jsonPath(treeList, "$..[?(@.name=='default')]")[0];
+
+                var ssdList = jsonPath(treeList, "$..[?(@.name=='ssd')]")[0];
+                var ssdOsdList= jsonPath(treeList, "$..[?(@.name=='ssd')]..osds[*].name");
+                if(ssdOsdList != undefined && ssdOsdList.length > 0){
+                  for(i=0;i< ssdOsdList.length;i++){
+                    var osdName= ssdOsdList[i];
+                    for(j=0;j< defaultList.hosts.length; j++){
+                        for(k=0;k< defaultList.hosts[j].osds.length; k++){
+                            if(osdName == defaultList.hosts[j].osds[k].name){
+                              defaultList.hosts[j].osds[k]['ceph_crush_name']="ssd";
+                            }
+                        }
+                    }
+                  }
+                }
+                var hddList = jsonPath(treeList, "$..[?(@.name=='hdd')]")[0];
+                var hddOsdList= jsonPath(treeList, "$..[?(@.name=='hdd')]..osds[*].name");
+                if(hddOsdList != undefined && hddOsdList.length > 0){
+                  for(i=0;i< hddOsdList.length;i++){
+                  var osdName= hddOsdList[i];
+                    for(j=0;j< defaultList.hosts.length; j++){
+                      for(k=0;k< defaultList.hosts[j].osds.length; k++){
+                              if(osdName == defaultList.hosts[j].osds[k].name){
+                              defaultList.hosts[j].osds[k]['ceph_crush_name']="hdd";
+                            }
+                        }
+                    }
+                  }
+                }
+                var tempDefaultList =[];
+                tempDefaultList[0]= defaultList;
+                osdList.topology= tempDefaultList;
                 osdList.cluster_status = jsonPath(dashApi.parseStorageHealthStatusData(status), "$.cluster_status")[0];
-                osdList.cluster_status.monitor_count= mons_total[0];
-                osdList.cluster_status.monitor_active= mons_active[0];
+                osdList.cluster_status.monitor_count= JSON.parse(mons_total);
+                osdList.cluster_status.monitor_active= JSON.parse(mons_active);
                 callback(osdList);
             });
-        });
+         });
     }
 }
 
 function parseMonitorWithHost(monsJSON, hostJSON){
-    var hstCnt= hostJSON.length;
-    for(i=0;i< hstCnt;i++){
+     var hstCnt= hostJSON.length;
+    for(i=0;i< hstCnt;i++){       
         var hostName= hostJSON[i].name;
         var monCnt = monsJSON.length;
         hostJSON[i].monitor = "Not Available";
@@ -119,7 +158,7 @@ function parseMonitorWithHost(monsJSON, hostJSON){
         hostJSON[i].kb_used = "Not Available";
         hostJSON[i].avail_percent= "Not Available";
         hostJSON[i].kb_avail= "Not Available";
-        for(j=0;j< monCnt; j++){
+       for(j=0;j< monCnt; j++){
             var monName= monsJSON[j].name;
             if(hostName == monName){
                 hostJSON[i].kb_total= monsJSON[j].kb_total;
@@ -128,60 +167,60 @@ function parseMonitorWithHost(monsJSON, hostJSON){
                 hostJSON[i].kb_avail= monsJSON[j].kb_avail;
                 hostJSON[i].monitor = monsJSON[j];
             }
-
+            
         }
     }
-    return hostJSON;
+   return hostJSON;
 }
 
 function parseRootFromHost(rootJSON, hostJSON){
-    var rootCnt = rootJSON.length;
-    for(q=0;q<rootCnt; q++) {
-        var total_up_node =0;
-        var total_down_node =0;
-        var total_warn_node =0;
+   var rootCnt = rootJSON.length;
+   for(q=0;q<rootCnt; q++) {
+       var total_up_node =0;
+       var total_down_node =0;
+       var total_warn_node =0;
 
-        var chldCnt = rootJSON[q].children.length;
-        for (i = 0; i < chldCnt; i++) {
-            var chldId = rootJSON[q].children[i];
-            var hostCnt = hostJSON.length;
-            for (j = 0; j < hostCnt; j++) {
-                var hostId = hostJSON[j].id;
-                if (chldId == hostId) {
-                    rootJSON[q].children[i] = hostJSON[j];
-                    var osdStatusJSON = jsonPath(hostJSON[j], "$.osds[*].status");
-                    var osdCnt = osdStatusJSON.length
-                    var hostStatus = "warn";
-                    var osdUp = 0, osdDown = 0;
-                    for (k = 0; k < osdCnt; k++) {
-                        var osdStatus = osdStatusJSON[k];
-                        if (osdStatus == "down") {
-                            osdDown = osdDown+1;
-                        }else if(osdStatus=="up"){
-                            osdUp= osdUp+1;
-                        }
-                    }
-                    if(osdDown == 0){
-                        hostStatus = "up";
-                    } else if(osdCnt ==osdDown){
-                        hostStatus = "down";
-                    }
-                    hostJSON[j].status = hostStatus;
-                    if (hostStatus == "up") {
-                        total_up_node = total_up_node + 1;
-                    } else if (hostStatus == "warn") {
-                        total_warn_node = total_warn_node + 1;
-                    } else{
-                        total_down_node = total_down_node + 1;
-                    }
-                }
-            }
-            rootJSON[q].total_node = chldCnt;
-            rootJSON[q].total_up_node = total_up_node;
-            rootJSON[q].total_warn_node = total_warn_node;
-            rootJSON[q].total_down_node = total_down_node;
-        }
-    }
+       var chldCnt = rootJSON[q].children.length;
+       for (i = 0; i < chldCnt; i++) {
+           var chldId = rootJSON[q].children[i];
+           var hostCnt = hostJSON.length;
+           for (j = 0; j < hostCnt; j++) {
+               var hostId = hostJSON[j].id;
+               if (chldId == hostId) {
+                   rootJSON[q].children[i] = hostJSON[j];
+                   var osdStatusJSON = jsonPath(hostJSON[j], "$.osds[*].status");
+                   var osdCnt = osdStatusJSON.length
+                   var hostStatus = "warn";
+                   var osdUp = 0, osdDown = 0;
+                   for (k = 0; k < osdCnt; k++) {
+                       var osdStatus = osdStatusJSON[k];
+                       if (osdStatus == "down") {
+                           osdDown = osdDown+1;
+                       }else if(osdStatus=="up"){
+                           osdUp= osdUp+1;
+                       }
+                   }
+                   if(osdDown == 0){
+                       hostStatus = "up";
+                   } else if(osdCnt ==osdDown){
+                       hostStatus = "down";
+                   }
+                   hostJSON[j].status = hostStatus;
+                   if (hostStatus == "up") {
+                       total_up_node = total_up_node + 1;
+                   } else if (hostStatus == "warn") {
+                       total_warn_node = total_warn_node + 1;
+                   } else{
+                       total_down_node = total_down_node + 1;
+                   }
+               }
+           }
+           rootJSON[q].total_node = chldCnt;
+           rootJSON[q].total_up_node = total_up_node;
+           rootJSON[q].total_warn_node = total_warn_node;
+           rootJSON[q].total_down_node = total_down_node;
+       }
+   }
     var jsonstr = JSON.stringify(rootJSON);
     var new_jsonstr = jsonstr.replace(/children/g, "hosts");
     rootJSON = JSON.parse(new_jsonstr);
