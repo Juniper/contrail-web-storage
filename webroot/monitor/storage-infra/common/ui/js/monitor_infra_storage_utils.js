1a25f2230907626783135e153e9146525c2cc9b6
Sarin Kizhakkepurayil <sarink@juniper.net>
2014-12-19 14:09:51 -0800

    Partial-Bug: #1404355 Issue: per host buid_info returns empty; breaks storage node infobox display Fix:     1. added check to verify length of the value in property build_info. response may be empty array or a string     2. if response is empty, version will be set to N/A Test:     checked by connecting API to HA cluster which has the issue noted.
    Change-Id: I66a3865e1ad26fdb731f87ab2bd911a39f38d449

--- webroot/monitor/storage-infra/common/ui/js/monitor_infra_storage_utils.js
+++ webroot/monitor/storage-infra/common/ui/js/monitor_infra_storage_utils.js
@@ -151,9 +151,15 @@ var infraMonitorStorageUtils = {
             obj['alerts'] = obj['nodeAlerts'].sort(dashboardUtils.sortInfraAlerts);
             //currently we are not tracking any storage process alerts.
             obj['processAlerts'] = [];
-            var versionArr = host['build_info'].split(" ");
-            obj['version'] = "Ceph " + versionArr[2];
-
+            /*
+            * build_info response holds version string or could be empty array.
+             */
+            if (host['build_info'].length < 0) {
+                var versionArr = host['build_info'].split(" ");
+                obj['version'] = "Ceph " + versionArr[2];
+            } else {
+                obj['version'] = "Ceph N/A";
+            }
             if (obj['color'] == d3Colors['red']) {
                 obj['downNodeCnt']++;
             }
