9f66fed88afb0dddb17b5a55d6e841d469184800
surakula <surakula@juniper.net>
2017-05-15 14:54:59 -0700

    Fixed: 1. Removed the local ceph rest api configuration 2. migrated ceph rest api server configuration to core contrail services
    Change-Id: Ide8e90164a2832b7581cad87ed903ee222fb3121
    Partial-Bug: #1688404

--- webroot/common/api/storage.rest.api.js
+++ webroot/common/api/storage.rest.api.js
@@ -2,59 +2,39 @@
  * Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
  */
 
-var storageConfig = require('../config/storage.config.global');
+var storageApi= require("./storage.api.constants");
 
-var storageApi= require('./storage.api.constants');
+var rest  = require(process.mainModule.exports.corePath + "/src/serverroot/common/rest.api"),
+    global = require(process.mainModule.exports.corePath + "/src/serverroot/common/global"),
+    assert = require("assert");
 
-var rest  = require(process.mainModule.exports["corePath"] + '/src/serverroot/common/rest.api'),
-    global = require(process.mainModule.exports["corePath"] + '/src/serverroot/common/global'),
-    assert = require('assert');
+var storageServer;
 
-var serverIp = global.DFLT_SERVER_IP;
-var serverPort = '5005';
-//var serverUrlVersion= storageApi.rest.serverVersion;
+storageServer = rest.getAPIServer({apiName:global.label.CEPH_REST_API_SERVER});
 
-
-if (storageConfig.ceph.server_ip) {
-    if (storageConfig.ceph.server_ip) {
-        serverIp = storageConfig.ceph.server_ip;
-    }
-    if (storageConfig.ceph.server_port) {
-        serverPort = storageConfig.ceph.server_port;
-    }
-}
-
-storageServer = rest.getAPIServer({apiName:global.label.CEPH_API_SERVER,
-                             server: serverIp, port: serverPort
-                             });
-
-function apiGet (url, appData, callback)
-{
+function apiGet (url, appData, callback) {
     var headers = {};
-    headers['Accept'] = 'application/json';
+    headers.Accept = "application/json";
     url = storageApi.rest.serverVersion + url;
-    
+
     storageServer.api.get(url, function(err, data) {
         callback(err, data);
     }, headers);
 }
 
-function apiPut (url, putData, appData, callback)
-{
+function apiPut (url, putData, appData, callback) {
     storageServer.api.get(url, putData, function(err, data) {
         callback(err, data);
     });
 }
 
-function apiPost (url, postData, appData, callback)
-{
+function apiPost (url, postData, appData, callback){
     storageServer.api.post(url, postData, function(err, data) {
         callback(err, data);
     });
 }
 
-function apiDelete (url, appData, allback)
-{
+function apiDelete (url, appData, callback) {
     storageServer.api.delete(url, function(err, data) {
         callback(err, data);
     });
@@ -64,4 +44,3 @@ exports.apiGet = apiGet;
 exports.apiPut = apiPut;
 exports.apiPost = apiPost;
 exports.apiDelete = apiDelete;
-
