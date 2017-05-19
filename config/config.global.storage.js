a961ad004e018c449fc59309d691babb545afaa8
surakula <surakula@juniper.net>
2017-05-19 00:05:16 -0700

    1. renamed storage.config.global.js to config.global.storage.js file 2. moved the config/config.global.storage.js to outside webroot folder     a. While installing the web-storage package we are going move this file /etc/contrail     b. we avoid to have duplicate this file while make dev-env and prod-env mode, as prod-env it will create a new dist folder
    Change-Id: I1d8c55726ed2088c44264556353e97b4e2a98c28
    Partial-Bug: #1688404

---
+++ config/config.global.storage.js
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+ */
+
+ /*****************************************************************************
+
+  * Ceph Rest API configuration
+
+  *****************************************************************************/
+ var config = {};
+
+ config.ceph = {};
+ config.ceph.server_ip = '127.0.0.1';
+ config.ceph.server_port = '5005';
+ config.ceph.statusURL = '/api/v0.1/status';
+
+ // Export this as a module.
+ module.exports = config;
